e6bff0d6e3edc522d886908c265e7f11b61365ae
daishi.yuan <daishi.yuan@weicheche.cn>
2019-03-27 19:36:47 +0800

    file-loader 学习

--- webpack.config.js
+++ webpack.config.js
@@ -22,7 +22,9 @@ module.exports={
     entry:config.entry,
     output:{
         filename:'[name].js?',
-        path:path.resolve(__dirname,'./app/dist')
+        path:path.resolve(__dirname,'./app/dist'),
+        // publicPath: 'dist/',//静态文件的路径，不设置的话，找不到（不是通过import引入的文件，如果是通过import引入的依赖，则只要第一个文件的路径正确即可，其他文件会被打包到这个文件里）
+        //写在这里的话，则全部的静态文件都会相对这个路径
     },
     module:{
         rules:[
@@ -41,7 +43,20 @@ module.exports={
             },
             {
                 test:/\.scss$/,
-                use:['style-loader', 'css-loader', 'sass-loader']//并没有scssloader，而且处理完了sass或者less之后，总是要使用css-loader处理，不然会报错
+                use:['style-loader','css-loader', 'sass-loader']//并没有scssloader，而且处理完了sass或者less之后，总是要使用css-loader处理，不然会报错
+            },
+            {
+                test:/\.(png|jpg|jepg)$/,
+                use:[
+                    {
+                        loader:'url-loader',//url-loader封装了file-loader，但url-loader并不依赖于file-loader。
+                        options:{
+                            limit:15360,
+                            outputPath:'img/',
+                            publicPath: 'dist/img',//静态文件的路径，不设置的话，找不到（不是通过import引入的文件，如果是通过import引入的依赖，则只要第一个文件的路径正确即可，其他文件会被打包到这个文件里）
+                        }
+                    }
+                ]
             }
         ]
     },
